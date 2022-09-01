94005cd5b3f18b2f8eaa07b4d2f65fc83f18a357
PikaPika0710 <102190298@sv1.dut.udn.vn>
2022-09-01 23:11:43 +0700

    colours and fonts

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-  runApp(MaterialApp(
-    home: Scaffold(
+void main() => runApp(MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-            title: const Text('My first project'),
-            centerTitle: true,
+          title: Text('My first project'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
         ),
-        body: const Center(
-          child: Text("Hello Viet Dev"),
+        body: Center(
+          child: Text(
+            "Hello Viet Dev",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower',
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => {},
-          child: const Text("Click"),
+          child: Text("Click"),
+          backgroundColor: Colors.red[600],
         ),
-  ),
-  ));
-
+      ),
+    ));
