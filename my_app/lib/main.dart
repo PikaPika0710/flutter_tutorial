6bb97974ba823fcec0e527134551cbb0d11fbe41
PikaPika0710 <102190298@sv1.dut.udn.vn>
2022-09-02 00:50:43 +0700

    images & assets

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
-));
-
+    ));
 
 class Home extends StatelessWidget {
   @override
@@ -15,16 +14,27 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          "Hello VietDev",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+        // child: Text(
+        //   "Hello VietDev",
+        //   style: TextStyle(
+        //     fontSize: 20.0,
+        //     fontWeight: FontWeight.bold,
+        //     letterSpacing: 2.0,
+        //     color: Colors.grey[600],
+        //     fontFamily: 'IndieFlower',
+        //   ),
+        // ),
+
+        // child: Image(
+        //     image:
+        //         // NetworkImage(
+        //         // 'https://images.wallpaperscraft.com/image/single/space_extraterrestrial_mountains_125431_320x480.jpg'),
+        //         // AssetImage('assets/images/universe-2.jpg'))
+        //     ),
+
+        // child: Image.asset('assets/images/universe.jpg'),
+        child: Image.network(
+            'https://images.wallpaperscraft.com/image/single/space_extraterrestrial_mountains_125431_320x480.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {},
