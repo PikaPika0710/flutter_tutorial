b2cf3cc4f29edda97382b5372505c50b3e5b05f6
PikaPika0710 <102190298@sv1.dut.udn.vn>
2022-09-04 13:58:00 +0700

    lists of data

---
+++ stateful_app/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa',
+    'When you reach the end of your rope, tie a knot in it and hang on. -Franklin D. Roosevelt',
+    'Always remember that you are absolutely unique. Just like everyone else. -Margaret Mead',
+    'Dont judge each day by the harvest you reap but by the seeds that you plant. -Robert Louis Stevenson',
+    'The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt',
+    'Tell me and I forget. Teach me and I remember. Involve me and I learn. -Benjamin Franklin',
+    'The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart. -Helen Keller',
+    'It is during our darkest moments that we must focus to see the light. -Aristotle',
+    'Whoever is happy will make others happy too. -Anne Frank',
+    'Do not go where the path may lead, go instead where there is no path and leave a trail. -Ralph Waldo Emerson'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Center(child: Text('Awesome Quotes')),
+          backgroundColor: Colors.amber,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Container(
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: quotes
+                    .map((quote) => Text(
+                          quote,
+                          style: TextStyle(fontSize: 15.0, color: Colors.white),
+                        ))
+                    .toList(),
+              )),
+        ));
+  }
+}
---
+++ stateful_app/lib/main_1.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatefulWidget {
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int level = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Center(child: Text('VietDev ID Card')),
+          backgroundColor: Colors.grey[800],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              level += 1;
+            });
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.red,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 40.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/avatar.jpg'),
+                  radius: 40.0,
+                ),
+              ),
+              Divider(height: 60.0, color: Colors.amber),
+              Text('NAME',
+                  style: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 12.0,
+                    letterSpacing: 2.0,
+                  )),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text('Tran Viet',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 20.0,
+                    letterSpacing: 1.0,
+                  )),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text('DOB',
+                  style: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 12.0,
+                    letterSpacing: 2.0,
+                  )),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text('07/10/2001',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 20.0,
+                    letterSpacing: 1.0,
+                  )),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text('ADDRESS',
+                  style: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 12.0,
+                    letterSpacing: 2.0,
+                  )),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text('Dien Ban - Quang Nam',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 20.0,
+                    letterSpacing: 1.0,
+                  )),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text('JOBS',
+                  style: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 12.0,
+                    letterSpacing: 2.0,
+                  )),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text('Fullstack Developer, Freelancer',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 20.0,
+                    letterSpacing: 1.0,
+                  )),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.email, color: Colors.blue, size: 20.0),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('trancongviet0710@gmail.com',
+                      style: TextStyle(
+                        color: Colors.yellow,
+                        fontSize: 20.0,
+                        letterSpacing: 1.0,
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text('LEVEL',
+                  style: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 12.0,
+                    letterSpacing: 2.0,
+                  )),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text('$level',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 20.0,
+                    letterSpacing: 1.0,
+                  )),
+            ],
+          ),
+        ));
+  }
+}
