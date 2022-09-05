f595711c300b4f7f894f6b487e1f803e58a84756
vietdev <trancongviet0710@gmail.com>
2022-09-05 16:57:53 +0700

    world time api

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -8,19 +8,20 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    Response response =
-        await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
-
+  void getTime() async {
+    Response response = await get(
+        Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Ho_Chi_Minh'));
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    DateTime now = DateTime.parse(data['datetime']);
+    now =
+        now.add(Duration(hours: int.parse(data['utc_offset'].substring(1, 3))));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
